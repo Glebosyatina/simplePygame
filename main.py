0e39e71a6c94c38f2b44ca2ae9089889800c060d
Gleb <barabarabara@bereberebere.com>
2025-10-20 15:23:41 +0300

    change game map and animals

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from pers import Pers
 
 pygame.init()
 # Setting game window dimensions
-window_width = 1280 
-window_height = 720
+window_width = 1920 
+window_height = 1080
 game_display = pygame.display.set_mode((window_width, window_height))
-
+pygame.display.set_caption('Неквиз')
 clock = pygame.time.Clock()
 
 # Loading the image
-bg_image = pygame.image.load('images/back.jpg').convert()
+bg_image = pygame.image.load('images/karta.png').convert()
 bg_image = pygame.transform.scale(bg_image, (window_width, window_height))
 
 # Main game loop
@@ -19,20 +19,56 @@ running = True
 #Pers
 active_pers = 0
 
-pers1 = Pers(game_display)
-pers2 = Pers(game_display)
-pers2.other_image('images/croc.png')
-pers3 = Pers(game_display)
-pers3.other_image('images/corgi.png')
-pers4 = Pers(game_display)
-pers4.other_image('images/shrek.png')
+#создаем персов и привязываем нужные изображения
+bear = Pers(game_display)
+
+cat = Pers(game_display)
+cat.other_image('images/cat.png')
+
+cow = Pers(game_display)
+cow.other_image('images/cow.png')
+
+dog = Pers(game_display)
+dog.other_image('images/dog.png')
+
+fox = Pers(game_display)
+fox.other_image('images/fox.png')
+
+koala = Pers(game_display)
+koala.other_image('images/koala.png')
+
+lion = Pers(game_display)
+lion.other_image('images/lion.png')
+
+monkey = Pers(game_display)
+monkey.other_image('images/monkey.png')
+
+mouse = Pers(game_display)
+mouse.other_image('images/mouse.png')
+
+tiger = Pers(game_display)
+tiger.other_image('images/tiger.png')
+
+sova = Pers(game_display)
+sova.other_image('images/sova.png')
+
+zayats = Pers(game_display)
+zayats.other_image('images/zayats.png')
+
 
 persons = list()
-persons.append(pers1)
-persons.append(pers2)
-persons.append(pers3)
-persons.append(pers4)
-
+persons.append(bear)
+persons.append(cat)
+persons.append(cow)
+persons.append(dog)
+persons.append(fox)
+persons.append(koala)
+persons.append(lion)
+persons.append(monkey)
+persons.append(mouse)
+persons.append(tiger)
+persons.append(sova)
+persons.append(zayats)
 
 while running:
     for event in pygame.event.get():
@@ -45,7 +81,11 @@ while running:
             my_mod = event.mod
             print("pressed:", {pygame.key.name(my_key)})
             print("mod:", {my_mod})
+			#выбор активного перса по нажатию цифры
             if event.key == pygame.K_1:
+                if active_pers == 1:
+                    active_pers = 11
+                    break
                 active_pers = 1
             if event.key == pygame.K_2:
                 active_pers = 2
@@ -53,10 +93,26 @@ while running:
                 active_pers = 3
             if event.key == pygame.K_4:
                 active_pers = 4
+            if event.key == pygame.K_5:
+                active_pers = 5
+            if event.key == pygame.K_6:
+                active_pers = 6
+            if event.key == pygame.K_7:
+                active_pers = 7
+            if event.key == pygame.K_8:
+                active_pers = 8
+            if event.key == pygame.K_9:
+                active_pers = 9
             if event.key == pygame.K_0:
+				#10 перс и тд, то есть если сначала была нажата 1 потом 0
+                if active_pers == 1:
+                    active_pers = 10
+                    break
                 active_pers = 0
+
     # Drawing image at position (0,0)
     game_display.blit(bg_image, (0, 0))
+
     for i in range(len(persons)):
         persons[i].update(active_pers)
     
--- pers.py
+++ pers.py
@@ -1,31 +1,30 @@
 import pygame
 from time import sleep
 
-
 num_pers = 0
-pers_initx = 950
-pers_inity = 670
+pers_initx = 465
+pers_inity = 605
 pers_width = 100
 pers_heigth = 60
 class Pers:
-	#по умолчанию капибара
+	#по умолчанию медведь
 	def __init__(self,window):
 		self.window = window
 		global points
 
-		self.image = pygame.transform.scale(pygame.image.load('images/kapibara.png').convert_alpha(), (pers_width,pers_heigth))
+		self.image = pygame.transform.scale(pygame.image.load('images/bear.png').convert_alpha(), (pers_width,pers_heigth))
 		self.rect  = self.image.get_rect(center=(pers_initx, pers_inity))
 
 		self.speed = 24  #скорость
 		self.cur_point = -1    #текущая позиция
 		global num_pers
-		self.num   = num_pers + 1 # номер перса
+		self.num   = num_pers # номер перса
 		num_pers   += 1		
 
 	#другой перс
 	def other_image(self, image_path):
 		self.image = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(), (pers_width, pers_heigth))
-		self.rect = self.image.get_rect(center=(pers_initx + 80*(num_pers-1), pers_inity))
+		self.rect = self.image.get_rect(center=(pers_initx + 100*(num_pers-1), pers_inity))
 		self.speed = 5
 
 	#num - номер активного перса	
@@ -35,20 +34,14 @@ class Pers:
 		keys = pygame.key.get_pressed()
 		#проверяем нажатые клавиши и двигаем перса
 		if self.num == num:
-			if keys[pygame.K_RIGHT] and self.rect.x < 1130:
-				self.rect.x += self.speed
-				print(self.num, "x:", self.rect.x, "y:", self.rect.y)
-			if keys[pygame.K_LEFT] and self.rect.x > 0:
-				self.rect.x -= self.speed	
-				print(self.num, "x:", self.rect.x, "y:", self.rect.y)
 			if keys[pygame.K_UP]:
-				if self.cur_point < 39:
+				if self.cur_point < 36:
 					self.cur_point += 1
 				self.rect.x = points[self.cur_point][0] - pers_width/2
 				self.rect.y = points[self.cur_point][1] - pers_heigth/2
 				#self.rect.y -= self.speed
 				print(self.num, "x:", self.rect.x, "y:", self.rect.y)
-				sleep(0.1)
+				sleep(0.2)
 			if keys[pygame.K_DOWN]:
 				if self.cur_point > 0:
 					self.cur_point -= 1
@@ -56,11 +49,12 @@ class Pers:
 				self.rect.y = points[self.cur_point][1] - pers_heigth/2
 				#self.rect.y += self.speed			
 				print(self.num, "x:", self.rect.x, "y:", self.rect.y)
-				sleep(0.1)
-		#отрисовка перса
+				sleep(0.2)
+        #отрисовка перса
 		self.window.blit(self.image, self.rect)	
 
+#координаты перебить
 points = [
-[1183, 599],[1090, 547],[1057, 463],[1109, 398],[1081, 312],[994, 295],[944, 362],[912, 449],[874, 529],[843, 621],[776, 666],[710, 598],[739, 517],[757, 429],[790, 341],[847, 283],[923, 246],[984, 178],[806, 169],[735, 230], [669, 282],[586, 312],[539, 378],[468, 432],[400, 501],[473, 536],[574, 549],[624, 628],[544, 666],[451, 669], [357, 654],[288, 586],[316, 507],[217, 521],[132, 504],[103, 412],[126, 326],[150, 234],[254, 218],[333, 142]
+[160, 668],[98, 540],[249, 500],[314, 376],[415, 340],[560, 361],[647, 272],[736, 200],[845, 176],[951, 212],[1055, 272],[1179, 201],[1279, 315],[1398, 214],[1500, 287],[1478, 476],[1581, 554],[1704, 465],[1812, 519],[1805, 748], [1679, 717],[1623, 852],[1541, 881],[1459, 955],[1324, 953],[1228, 878],[1209, 710],[1102, 656],[979, 657],[939, 818], [859, 872],[770, 925],[635, 944],[511, 885],[428, 811],[352, 772],[240, 764]
 ]
 		
